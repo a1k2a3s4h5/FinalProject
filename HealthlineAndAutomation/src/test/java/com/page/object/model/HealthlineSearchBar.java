99cd2fc8dea3fc476107e37a5fb8fa084c160dfe
joukanivanshika16 <74662920+joukanivanshika16@users.noreply.github.com>
2021-04-01 19:09:51 +0530

    HealthLine SearchBar completed POM.

---
+++ HealthlineAndAutomation/src/test/java/com/page/object/model/HealthlineSearchBar.java
@@ -0,0 +1,21 @@
+package com.page.object.model;
+
+import org.openqa.selenium.WebDriver;
+
+
+import com.healthline.locators.SearchBarLocators;
+
+public class HealthlineSearchBar {
+	public WebDriver driver;
+	public SearchBarLocators searchBar;
+		
+		public HealthlineSearchBar(WebDriver driver) {	
+			this.driver=driver;
+			searchBar=new SearchBarLocators(driver);
+		}
+	
+	public void workingOfSearchBar() {
+		searchBar.searchBox.sendKeys("Women Health");
+		searchBar.searchElement.click();
+	}
+}
